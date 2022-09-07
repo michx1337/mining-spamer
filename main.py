551f03e6469bcd8261bd6a42712ba87930eb0d45
michx <96200291+michx1337@users.noreply.github.com>
2022-09-07 00:55:49 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from pynput.keyboard import Key, Controller
+import time
+
+kb = Controller()
+time.sleep(1)
+toez = '.mines lol-nice-bot-ur-fucking-gay-u-fuckkk'
+for x in range(100):
+    time.sleep(0.8)
+    kb.type(toez)
+    kb.press(Key.enter)
